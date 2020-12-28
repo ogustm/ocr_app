fc9cd278365dede8e5294024955feb571a04244a
ogustm <osward.gust.martinez@gmail.com>
2020-12-28 16:50:37 +0100

    upload logic

--- app.js
+++ app.js
@@ -10,3 +10,16 @@ const multer = require("multer");
 const { TesseractWorker } = require("tesseract.js");
 // To analize our images
 const worker = new TesseractWorker();
+
+//To save uploaded images
+
+const storage = multer.diskStorage({
+    destination: (req, res, cb) => {
+        cb(null, "./uploads")
+    },
+    filename: (req, res, cb) => {
+        cb(null, req.file);
+    }
+});
+
+const upload = multer({storage: storage}).single("avatar");
