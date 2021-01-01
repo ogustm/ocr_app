e8894c7e7983cd265bc2cf903bda0156cf5d03c1
ogustm <osward.gust.martinez@gmail.com>
2021-01-01 18:55:07 +0100

    reading text from image

--- app.js
+++ app.js
@@ -19,10 +19,12 @@ const worker = createWorker();
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "./uploads")
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
     }
 });
 const upload = multer({storage: storage}).single("avatar");
-
 
 app.set("view engine", "ejs");
 
@@ -33,8 +35,19 @@ app.get("/", (req, res) => {
 
 app.post('/upload', (req, res) => {
     upload(req, res, error => {
-        console.log(req.file);        
-    })
+      fs.readFile(`./uploads/${req.file.originalname}`, (err, data) => {
+        if(error) return console.log('This is your error', err);
+        
+        (async () => {
+            await worker.load();
+            await worker.loadLanguage('eng');
+            await worker.initialize('eng');
+            const { data: { text } } = await worker.recognize(data);
+            console.log(text);
+            await worker.terminate();
+          })();
+      });         
+    });
 })
 
 //Start Up the Server
