df9870f82f6d50239b3f1c9c32244782eb0078cf
ogustm <osward.gust.martinez@gmail.com>
2021-01-05 12:00:08 +0100

    it works

--- app.js
+++ app.js
@@ -36,22 +36,41 @@ app.get("/", (req, res) => {
 })
 
 app.post('/upload', (req, res) => {
+
     upload(req, res, error => {
-      fs.readFile(`./uploads/${req.file.originalname}`, (err, data) => {
+      fs.readFile(`./uploads/${req.file.originalname}`, (err, imageData) => {
         if(error) return console.log('This is your error', err);
         
         (async () => {
             await worker.load();
             await worker.loadLanguage('eng');
             await worker.initialize('eng');
-            const { data: { text } } = await worker.recognize(data, "eng", {tessjs_create_pdf: '1'});
-            res.send(text);                    
+            const { data: { text } } = await worker.recognize(imageData);            
+            
+            const pdfPromise = await worker.getPDF();
+            
+            const regex = /\.gif|\.jpg|\.png|\.webg|\.jpeg/gi;
+            let fileName = req.file.originalname;
+            fileName = fileName.replace(regex, '.pdf');
+
+            fs.writeFileSync(fileName, Buffer.from(pdfPromise.data));
+            
+            res.redirect("/download?fileName=" + fileName);
+            
           })()
           
       });
                
     });
-})
+});
+
+app.get("/download", (req, res) => {
+
+    const file = `${__dirname}/${req.query.fileName}`;
+    res.download(file);
+});
+
+
 
 //Start Up the Server
 const PORT = 5000 || process.env.PORT;
